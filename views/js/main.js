b0bb7f8e769ab8cf4c0b33a34446fc57350b69df
GVBilokorenko <abilokorenko@gmail.com>
2021-02-25 22:39:38 +0200

    Quick update

--- views/js/main.js
+++ views/js/main.js
@@ -1,2 +1,11 @@
-"use strict"
+"use strict";
 
+function login() {
+	document.getElementById("register").style.left = "650px";
+	document.getElementById("login").style.left = "0";
+}
+
+function register() {
+	document.getElementById("register").style.left = "0px";
+	document.getElementById("login").style.left = "-650px";
+}
